031b57e95d1230d5621b15c71b5f5ca948e8c475
Rodevp <rodriguezrodrigor12@gmail.com>
2022-06-13 21:04:18 -0500

    agregando js para la logica

---
+++ app.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll('.slide')
+const dots = document.querySelectorAll('.dot')
+
+function setActiveSlide(i) {
+    
+    for (let slide of slides) {
+        console.log('slide -> ', slide)
+        slide.classList.remove('active')
+        slides[i].classList.add('active')
+    }
+
+    for (let dot of dots) {
+        console.log('slide -> ', dot)
+        dot.classList.remove('active')
+        dots[i].classList.add('active')
+    }
+
+}
+
+for (let j = 0; j < dots.length; j++) {
+    dots[j].addEventListener('click', e => {
+        setActiveSlide(j)
+    })
+}
