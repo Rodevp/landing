5af35b99ade26f24159d33b70f88e427f4d24809
Rodevp <rodriguezrodrigor12@gmail.com>
2022-07-26 23:19:03 -0500

    remove console logs

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const dots = document.querySelectorAll('.dot')
 function setActiveSlide(i) {
     
     for (let slide of slides) {
-        console.log('slide -> ', slide)
         slide.classList.remove('active')
         slides[i].classList.add('active')
     }
 
     for (let dot of dots) {
-        console.log('slide -> ', dot)
         dot.classList.remove('active')
         dots[i].classList.add('active')
     }
